028281c9a0057d604f947ac1b96e2668c46c58cf
Rozelin <rozelin.dc@gmail.com>
2022-02-24 23:22:48 +0900

    router導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 import Counter from './Counter'
@@ -8,31 +9,12 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <Counter />
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Router>
+          <Routes>
+            <Route exact path="/"></Route>
+            <Route path="/counter" element={<Counter />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   )
